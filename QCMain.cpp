22390a52048dcd1d10def01175ef3f9c58497177
Spencer <Spencer@macbook-pro.lan>
2020-04-05 18:20:12 -0600

    Added comments to QC.cpp

--- QCMain.cpp
+++ QCMain.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 
 int main(int argv, char** argc){
+  // create QC object and great myself!
   QC qc;
   std::cout << "\n";
   std::cout << "Hello, Spencer!" << "\n"; 
+  
+  // use my methods
   qc.todays_date();
   qc.days_spent_in_quarantine();
   return 0;
