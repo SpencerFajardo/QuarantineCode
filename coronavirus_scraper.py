2e015af315f589286a3c0d78cfa7cf18ff9f3154
Spencer <Spencer@macbook-pro.lan>
2020-04-05 18:17:51 -0600

    Added comments to coronavirus_scraper.py

--- coronavirus_scraper.py
+++ coronavirus_scraper.py
@@ -2,16 +2,22 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
+# This is the main method, that tells the scraper to run.
 def main():
    get_infection_information()
 
+# This is the method that does the heavy lifting.
+# It scrapes a wikipedia page with a table of coronavirus
+# cases per country. Then, it adds all the cases up 
+# and prints the value.
 def get_infection_information():
-   # We have to find out what the url is for the JHU interactive map.
+   # This is the url for the wiki page with the necessary information
    url = "https://en.wikipedia.org/wiki/Template:2019–20_coronavirus_pandemic_data" 
    
    # Requests allows us to search for url
    r = requests.get(url)
    soup = BeautifulSoup(r.content, 'html.parser')
+   # This searches through the soup to find the tables
    tables = soup.find_all('table')
 
    #Create array to hold the data we extract
@@ -25,17 +31,26 @@ def get_infection_information():
         
         
             if len(cells) > 1:
+               # cells[0] holds the case counts
                case = cells[0]
+
+               # I had some issues here. For some reason
+               # I was getting the cases information,
+               # and then some additional stuff afterwards.
+               # My fix was to add things until the process_num
+               # funtion didn't work, and then to exit the loop.
                try:
                   cases.append(process_num(case.text.strip()))
                except ValueError:
                   break
+   # Add all cases, and then print
    total = 0
    for case in cases:
       total = total + case
    print("Coronavirus Case World Total: " + str(total))
 
-
+# This method processes the strings from the table, and prepares
+# them to be just numbers
 def process_num(num):
    return float(re.sub(r'[^\w\s.]','',num))
 
